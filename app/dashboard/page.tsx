faa6092d28c73012a8a70d2d8b42131c6dd11caa
SaferAttendance <eric@reddoormarketingco.com>
2025-07-22 23:37:34 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -66,7 +66,7 @@ export default function AdminDashboard() {
           />
         </svg>
       ),
-      onClick: () => router.push("/dashboard/teachers/page.tsx"),
+      onClick: () => router.push("/dashboard/teachers"),
     },
     {
       id: "students",
