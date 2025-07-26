b33972d6bd3485d7680324f8346727fe35b93818
SaferAttendance <eric@reddoormarketingco.com>
2025-07-25 23:23:53 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
-import '../globals.css';
+// app/layout.tsx
+import '../globals.css'
+import { ReactNode } from 'react'
+
+export const metadata = {
+  title: 'Safer Attendance',
+  description: 'Ensuring Safety One Class At A Time While Promoting Attendance',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
